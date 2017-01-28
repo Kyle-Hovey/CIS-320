848f1690d73059349ab2684711f737e3f1a74dc5
Kyle Hovey <kyle.hovey@my.simpson.edu>
2017-01-28 12:30:48 -0600

    Assignment 3 Commit

---
+++ CIS-320-First-Project/src/edu/simpson/cis320firstproject/NameListGet.java
@@ -0,0 +1,32 @@
+package edu.simpson.cis320firstproject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import com.google.gson.Gson;
+
+/**
+ * Created by Kyle on 1/26/2017.
+ */
+@WebServlet(name = "NameListGet")
+public class NameListGet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(PersonDAO.getPeople());
+
+        out.print(json);
+    }
+}
---
+++ CIS-320-First-Project/src/edu/simpson/cis320firstproject/Person.java
@@ -0,0 +1,23 @@
+package edu.simpson.cis320firstproject;
+
+/**
+ * Created by Kyle on 1/26/2017.
+ */
+public class Person {
+    private int id;
+    private String first;
+    private String last;
+    private String phone;
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirst() {return first; }
+    public void setFirst(String first) {this.first = first; }
+
+    public String getLast() {return last; }
+    public void setLast(String last) {this.last = last; }
+
+    public String getPhone() {return phone; }
+    public void setPhone(String phone) {this.phone = phone; }
+}
