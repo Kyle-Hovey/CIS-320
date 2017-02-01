b1cdac0e808fde0d7bd1a9072293b706ec12e81c
Kyle Hovey <kyle.hovey@my.simpson.edu>
2017-01-31 18:21:48 -0600

    Assignment 4 Commit

--- CIS-320-First-Project/web/js/name_list.js
+++ CIS-320-First-Project/web/js/name_list.js
@@ -9,15 +9,16 @@ function updateTable() {
             // json_result is an object. You can set a breakpoint, or print
             // it to see the fields. Specifically, it is an array of objects.
             // Here we loop the array and print the first name.
-            if (json_result.length > 0) {
-                $('#datatable tbody').remove();
-
                 for (var i = 0; i < json_result.length; i++) {
                     $('#datatable tbody').append("<tr><td>" + json_result[i].first + "</tr></td>");
                 }
-                console.log("Done");
-            }
-            else console.log("No Data to Add.");
+
+                if (json_result.length != 0) {
+                    $('#noData').remove();
+                    console.log("Done.");
+                }
+
+                else console.log("No data to append.")
         }
     )
 }
